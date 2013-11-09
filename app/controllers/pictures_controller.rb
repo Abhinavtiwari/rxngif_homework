8575f7d13ab1bea8f4edf70f93a45f565e518abf
Abhinav Tiwari <abhinavt82@gmail.com>
2013-11-09 15:02:17 -0600

    Completed once with all changes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,4 +6,33 @@ class PicturesController < ApplicationController
  def index
   	@list_of_pictures = Picture.all
   end
+ 
+
+ def create
+ 	p = Picture.new
+ 	p.source = params[:source]
+ 	p.caption = params[:caption]
+ 	p.save
+ 	redirect_to pictures_url, :notice => "Picture created"
  end
+
+def destroy
+	p = Picture.find(params[:id]) 
+	p.destroy 
+	redirect_to pictures_url, :notice => "Picture deleted"
+end
+
+def edit
+	@picture = Picture.find(params[:id]) 
+end
+
+def update
+	p = Picture.find(params[:id]) 
+	p.source = params[:source]	
+	p.caption = params[:caption]
+	p.save
+	redirect_to picture_url(p.id), :notice => "Picture updated"
+end
+
+
+end
